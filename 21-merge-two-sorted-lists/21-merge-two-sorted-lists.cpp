0a704e7fdd30af9c3ec69938d701ad82d56f3bf1
keshav-tyagi28 <89222405+keshav-tyagi28@users.noreply.github.com>
2022-05-10 10:41:27 +0530

    Time: 3 ms (97.46%), Space: 14.9 MB (44.23%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -19,19 +19,82 @@ public:
         else if(list2==NULL)
             return list1;
         
-         ListNode* newhead=NULL;
-        
-        if(list1->val<=list2->val){
-            
-            newhead=list1;
-            newhead->next=mergeTwoLists(list1->next,list2);
+        ListNode* newhead=NULL;
+        ListNode* newtail=NULL;
+        ListNode* t1=list1;
+        ListNode* t2=list2;
+        if(t1->val<=t2->val)
+        {
+            newhead=t1;
+            newtail=t1;
+            t1=t1->next;
+
         }
-       else if(list1->val>list2->val){
-            
-            newhead=list2;
-            newhead->next=mergeTwoLists(list1,list2->next);
+        
+        else if(t1->val>t2->val)
+        {
+            newtail=t2;
+            newhead=t2;
+            t2=t2->next;
+   
         }
         
-        return newhead;
-    }
+        while(t1!=  NULL && t2!=NULL)
+        {
+            if(t1->val<=t2->val)
+        {
+            
+            newtail->next=t1;
+              newtail=t1;  
+            t1=t1->next;
+
+        }
+        
+        else if(t1->val>t2->val)
+        {
+            newtail->next=t2;
+            newtail=t2;
+            t2=t2->next;
+   
+        }
+            
+            
+            
+        }
+
+        
+        while(t1!=NULL)
+        {
+             newtail->next=t1;
+              newtail=t1;  
+            t1=t1->next;
+        }
+        
+            
+        while(t2!=NULL)
+        {
+             newtail->next=t2;
+              newtail=t2;  
+            t2=t2->next;
+        }
+        
+        return newhead;}
+        
+        
+    
+
+        
+//         if(list1->val<=list2->val){
+            
+//             newhead=list1;
+//             newhead->next=mergeTwoLists(list1->next,list2);
+//         }
+//        else if(list1->val>list2->val){
+            
+//             newhead=list2;
+//             newhead->next=mergeTwoLists(list1,list2->next);
+//         }
+        
+//         return newhead;
+//     }
 };
