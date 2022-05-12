b92e4dcd6b3fb8266aaba9dfe03434d839b7f9ef
keshav-tyagi28 <89222405+keshav-tyagi28@users.noreply.github.com>
2022-05-12 11:12:41 +0530

    Time: 21 ms (53.20%), Space: 11.9 MB (13.28%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -2,53 +2,47 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
-
+        vector<int> ans;
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++)
+            m[nums[i]]=i;
+        
+         for(int i=0;i<nums.size();i++)
+            {
+             
+             int r=target-nums[i];
+             if(m.find(r)!=m.end() && m[r]!=i)
+             {
+                  ans.push_back(m[r]);
+                    ans.push_back(i);
+                    return ans;
+             }
+         }
+        return ans;}
+                
         
         
-//         int e=nums.size()-1;
-//         vector<int> ans;
-        // unordered_map<int,int> m;
-//         for(int i=0;i<=e;i++)
-//             m[nums[i]]=i;
+        //one loop work
+        
+//          unordered_map<int,int> m;
+//                 vector<int> ans;
 
-//         for(auto x:m)
+//         for(int i=0;i<nums.size();i++)
 //         {
-//             int c=x.second;
-//             for(int i=c+1;i<=e;i++)
+//             if(m.find(target-nums[i])!=m.end())
 //             {
-//                 if(nums[i]+(x.first)==target)
-//                 {
-//                             ans.push_back(x.second);
-//                              ans.push_back(i);
-//                                  return ans;
-//                  }
+//                 ans.push_back(i);
+//                 ans.push_back(m[target-nums[i]]);
+//                 return ans;
                 
 //             }
-          
+//             m[nums[i]]=i;
+            
 //         }
         
 //         return ans;
+        
+        
 //     }
-        
-         unordered_map<int,int> m;
-                vector<int> ans;
-
-        for(int i=0;i<nums.size();i++)
-        {
-            if(m.find(target-nums[i])!=m.end())
-            {
-                ans.push_back(i);
-                ans.push_back(m[target-nums[i]]);
-                return ans;
-                
-            }
-            m[nums[i]]=i;
-            
-        }
-        
-        return ans;
-        
-        
-    }
         
 };
