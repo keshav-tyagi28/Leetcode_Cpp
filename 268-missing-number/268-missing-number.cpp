484c94a72edb56680473c5439564253835843e65
keshav-tyagi28 <89222405+keshav-tyagi28@users.noreply.github.com>
2022-06-02 10:11:25 +0530

    Time: 27 ms (56.05%), Space: 18 MB (64.36%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -2,18 +2,29 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         
-        unordered_map<int,int> m;
-        for(int i=0;i<nums.size();i++)
-        {
-           m[nums[i]]++;
-        }
+//         unordered_map<int,int> m;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//            m[nums[i]]++;
+//         }
         
-        for(int i=0;i<=nums.size();i++)
-        {
-            if(m.find(i)==m.end())
-                return i;
-        }
+//         for(int i=0;i<=nums.size();i++)
+//         {
+//             if(m.find(i)==m.end())
+//                 return i;
+//         }
         
-        return -1;
+//         return -1;
+        
+        
+        int sum=0,arrsum=0;
+            for(int i=0;i<=nums.size();i++)
+            sum+=i;
+        
+          for(int i=0;i<nums.size();i++)
+            arrsum+=nums[i];
+        
+        return sum-arrsum;
+        
     }
 };
