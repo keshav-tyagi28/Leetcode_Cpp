a2511c896accfb6a43e9698c335cc1cd675d334f
keshav-tyagi28 <89222405+keshav-tyagi28@users.noreply.github.com>
2022-06-02 09:57:37 +0530

    Time: 22 ms (19.54%), Space: 10.5 MB (78.16%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -1,45 +1,59 @@
 class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
-       
-        if(matrix.size()!=matrix[0].size())
-        {
-            vector<vector<int>> ans(matrix[0].size(),vector<int> (matrix.size()));
-            
-            int i=0;
-            int r=0,c=0,col=0;
-            while(col<matrix[0].size())
-            {
-                for(int j=0;j<matrix.size();j++)
-                {
-                    ans[r][c]=matrix[j][i];
-                    c++;
-                }
-                col++;
-                i++;
-                c=0;
-                r++;
-            }
-            return ans;
-            
-        }        
         
         
+               int n = matrix.size();
+        int m = matrix[0].size();
         
-        
-     
-        for(int i=0;i<matrix.size();i++)
-        {
-            for(int j=i;j<matrix[i].size();j++)
-            {
-                int tmp=matrix[i][j];
-                matrix[i][j]=matrix[j][i];
-                matrix[j][i]=tmp;
-                
+        vector<vector<int>> res(m,vector<int> (n,0)); // matrix creation
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                res[j][i] = matrix[i][j]; // filling the matrix in transpose form
             }
         }
+        return res;
         
-        return matrix;
+        
+       
+//         if(matrix.size()!=matrix[0].size())
+//         {
+//             vector<vector<int>> ans(matrix[0].size(),vector<int> (matrix.size()));
+            
+//             int i=0;
+//             int r=0,c=0,col=0;
+//             while(col<matrix[0].size())
+//             {
+//                 for(int j=0;j<matrix.size();j++)
+//                 {
+//                     ans[r][c]=matrix[j][i];
+//                     c++;
+//                 }
+//                 col++;
+//                 i++;
+//                 c=0;
+//                 r++;
+//             }
+//             return ans;
+            
+//         }        
+        
+        
+        
+//         //when size is same
+     
+//         for(int i=0;i<matrix.size();i++)
+//         {
+//             for(int j=i;j<matrix[i].size();j++)
+//             {
+//                 int tmp=matrix[i][j];
+//                 matrix[i][j]=matrix[j][i];
+//                 matrix[j][i]=tmp;
+                
+//             }
+//         }
+        
+//         return matrix;
     
                
 
