df158ad71335bd3087c108a3dc0c1607d6d2161d
keshav-tyagi28 <89222405+keshav-tyagi28@users.noreply.github.com>
2022-06-03 14:15:46 +0530

    Time: 1106 ms (5.01%), Space: 17.5 MB (78.46%) - LeetHub

--- 455-assign-cookies/455-assign-cookies.cpp
+++ 455-assign-cookies/455-assign-cookies.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-//      int count=0;
+     int count=0;
         
-//       sort(g.begin(),g.end());
-//               sort(s.begin(),s.end());  
+      sort(g.begin(),g.end());
+              sort(s.begin(),s.end());  
 
-//         for(int i=0;i<g.size();i++)
-//         {
-//             for(int j=0;j<s.size();j++)
-//             {
-//                 if(s[j]>=g[i])
-//                 {
-//                     count++;
-//                     s[j]=-1;
-//                     break;
-//                 }
-//             }
-//         }
-        
-//         return count;
-        sort(g.begin(), g.end());
-        sort(s.begin(), s.end());
-        
-        int i = 0, j = 0, ans = 0;
-        
-        while(i < g.size() && j < s.size()) {
-            if(g[i] <= s[j]) ans++, i++;
-            j++;
+        for(int i=0;i<g.size();i++)
+        {
+            for(int j=0;j<s.size();j++)
+            {
+                if(s[j]>=g[i])
+                {
+                    count++;
+                    s[j]=-1;
+                    break;
+                }
+            }
         }
         
-        return ans;
+        return count;
+//         sort(g.begin(), g.end());
+//         sort(s.begin(), s.end());
+        
+//         int i = 0, j = 0, ans = 0;
+        
+//         while(i < g.size() && j < s.size()) {
+//             if(g[i] <= s[j]) ans++, i++;
+//             j++;
+//         }
+        
+//         return ans;
         
         
     }
