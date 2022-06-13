beca5566aa3b045b21cd23761ff7343247f78fd2
keshav-tyagi28 <89222405+keshav-tyagi28@users.noreply.github.com>
2022-06-14 01:08:18 +0530

    Time: 32 ms (44.61%), Space: 7.6 MB (84.04%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        
+         unordered_map<char, int> mp;
+        int np = t.length();
+        int ns = s.length();
+        for (int i = 0; i < np; i++)
+        {
+            mp[t[i]]++;
+        }
+        int count = mp.size();
+        int i = 0;
+        int j = 0;
+        int len = INT_MAX;
+        pair<int, int> ans;
+        while (j < ns)
+        {
+            if (mp.find(s[j]) != mp.end())
+            {
+                mp[s[j]]--;
+                if (mp[s[j]] == 0)
+                {
+                    count--;
+                }
+                if (count == 0)
+                {
+                    while (count == 0)
+                    {
+                        if (j - i + 1 < len)
+                        {
+                            len = j - i + 1;
+                            ans = {i, len};
+                        }
+                        if (mp.find(s[i]) != mp.end())
+                        {
+                            mp[s[i]]++;
+                            if (mp[s[i]] == 1)
+                            {
+                                count++;
+                            }
+                        }
+                        i++;
+                    }
+                }
+            }
+            j++;
+        }
+        return (len == INT_MAX ? "" : s.substr(ans.first, ans.second));
+    }
+    
+};
