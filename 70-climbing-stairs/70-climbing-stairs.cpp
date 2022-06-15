f999e06311642c673aa5994db8a0304fb5e124b6
keshav-tyagi28 <89222405+keshav-tyagi28@users.noreply.github.com>
2022-06-15 11:16:04 +0530

    Time: 3 ms (35.21%), Space: 6.3 MB (15.11%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    
+   
+    int climbStairs(int n) {
+        
+       
+        
+       
+        vector<int> ans(n+1);
+        ans[0]=ans[1]=1;
+        
+        for(int i=2;i<=n;i++)
+            ans[i]=ans[i-1]+ans[i-2];
+        
+        return ans[n];
+        
+    }
+};
