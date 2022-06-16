f6fb07e091bf562e7ee5a3410ff0a147dc8d0261
keshav-tyagi28 <89222405+keshav-tyagi28@users.noreply.github.com>
2022-06-16 11:38:56 +0530

    Time: 237 ms (13.05%), Space: 14.5 MB (37.02%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    int solvetab(vector<int> &num, int x)
+{
+    vector<int> dp(x+1,INT_MAX);
+    dp[0]=0;
+    for(int i=1;i<=x;i++)
+    {
+        for(int j=0;j<num.size();j++)
+        {
+            if(i-num[j]>=0 && dp[i-num[j]]!=INT_MAX)
+                dp[i]=min(dp[i],1+dp[i-num[j]]);
+        }
+        
+    }
+ 
+    if(dp[x]==INT_MAX)
+        return -1;
+    else
+    return dp[x];
+
+}
+    int coinChange(vector<int>& coins, int amount) {
+        return solvetab(coins,amount);
+    }
+};
