9cf43d438f474344f280b5c3bad68d4cb1f180f5
keshav-tyagi28 <89222405+keshav-tyagi28@users.noreply.github.com>
2022-07-06 23:53:20 +0530

    Time: 16 ms (30.47%), Space: 5.9 MB (80.05%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,57 +1,15 @@
 class Solution {
 public:
-    int fibhelper(int n,int *ans)
-    {
-          if(n<=1)
-        {     ans[n]=n;  
-              return n;
-         }
-        
-        if(ans[n-1]==-1)
-        {
-            ans[n-1]=fibhelper(n-1,ans);
-        }
-       
-         if(ans[n-2]==-1)
-        {
-            ans[n-2]=fibhelper(n-2,ans);
-        }
-        
-        
-        return  ans[n-1]+ans[n-2];
-    }
     
     int fib(int n) {
-       
-//         int *ans=new int[n+1];
+    
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
         
-//         for(int i=0;i<n+1;i++)
-//         {
-//             ans[i]=-1;
-//         }
-            
-//      return fibhelper(n,ans);
+        int ans= fib(n-1)+fib(n-2);
         
-        if(n==0||n==1)
-            return n;
-            
-        
-        int remain=n-2;
-        remain+=1;
-        int a=0,b=1;
-        int count=1;
-        int ans;
-        while(count<=remain)
-        {
-            ans=a+b;
-            a=b;
-            b=ans;
-            count++;
-            
-        }
         return ans;
-      
-        
-        
-        
-}};
+    }
+};
