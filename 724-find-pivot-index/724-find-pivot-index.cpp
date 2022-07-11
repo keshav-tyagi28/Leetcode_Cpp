2aaf64ffafd7f999e5d44c04bb5e1f191030be07
keshav-tyagi28 <89222405+keshav-tyagi28@users.noreply.github.com>
2022-07-11 10:14:02 +0530

    Time: 27 ms (86.34%), Space: 31.2 MB (54.48%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -1,43 +1,58 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        
-        vector<pair<int,int>> ans;
+       
         int tsum=0;
+         for(int i=0;i<nums.size();i++)
+            tsum=tsum+nums[i];
         
+        
+        int cur=0;
         for(int i=0;i<nums.size();i++)
         {
-            tsum=tsum+nums[i];
+            int left=cur;
+            cur+=nums[i];
+            int next=tsum-cur;
+            if(next==left)
+                return i;
+            
         }
+        return -1;
+    }
         
-        int p=0;
-        int n=tsum-nums[0];
-        cout<<n;
-        ans.push_back(make_pair(p,n));
+//         vector<pair<int,int>> ans;
+//         int tsum=0;
+        
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             tsum=tsum+nums[i];
+//         }
+        
+//         int p=0;
+//         int n=tsum-nums[0];
+//         cout<<n;
+//         ans.push_back(make_pair(p,n));
         
      
 
         
-        for(int i=1;i<nums.size();i++)
-        {
-           int prev=ans[i-1].first+nums[i-1];
-           int next=tsum-prev-nums[i];
-            // cout<<prev<<" "<<next<<endl;
-          ans.push_back(make_pair(prev,next));
-                   
-
-
-            
-        }
+//         for(int i=1;i<nums.size();i++)
+//         {
+//            int prev=ans[i-1].first+nums[i-1];
+//            int next=tsum-prev-nums[i];
+//             // cout<<prev<<" "<<next<<endl;
+//           ans.push_back(make_pair(prev,next));
+          
+//         }
         
-        for(int i=0;i<ans.size();i++)
-        {
+//         for(int i=0;i<ans.size();i++)
+//         {
            
             
-            if(ans[i].first==ans[i].second)
-                return i;
-        }
+//             if(ans[i].first==ans[i].second)
+//                 return i;
+//         }
         
-        return -1;
-        }   
+//         return -1;
+//         }   
 };
