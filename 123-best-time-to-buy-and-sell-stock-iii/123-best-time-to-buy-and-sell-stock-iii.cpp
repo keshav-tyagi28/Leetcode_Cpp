b27760710d87fe6c8314715ae04abf9c4a3d56c8
keshav-tyagi28 <89222405+keshav-tyagi28@users.noreply.github.com>
2022-07-14 21:01:06 +0530

    Time: 1381 ms (8.25%), Space: 207 MB (30.04%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -29,57 +29,59 @@ public:
         // vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(3,-1))); 
         // return f(0,prices,1,dp,2);
         
-//        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0))); 
+       vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0))); 
   
-//         for(int ind = n-1; ind>=0; ind--){
+        for(int ind = n-1; ind>=0; ind--){
+        for(int buy = 0; buy<2; buy++){
+            for(int cap=1; cap<=2; cap++){    // dont start from 0 as it is covered already also
+                                            // there is cap-1 in if therefore runtiem error
+                
+                int profit=0;
+                if(buy==0){// We can buy the stock
+                    profit = max(0+dp[ind+1][0][cap], 
+                                -prices[ind] + dp[ind+1][1][cap]);
+                 }
+    
+                if(buy==1){// We can sell the stock
+                    profit = max(0+dp[ind+1][1][cap],
+                                prices[ind] + dp[ind+1][0][cap-1]); 
+                    
+                }
+                                    dp[ind][buy][cap]=profit;
+
+            }
+        }
+    }
+        return dp[0][0][2];
+        
+        
+//         vector<vector<int>> ahead(2,vector<int>(3,0));
+//         vector<vector<int>> cur(2,vector<int>(3,0));
+
+//           for(int ind = n-1; ind>=0; ind--){
 //         for(int buy = 0; buy<2; buy++){
 //             for(int cap=1; cap<=2; cap++){
                 
 //                 int profit=0;
 //                 if(buy==0){// We can buy the stock
-//                     profit = max(0+dp[ind+1][0][cap], 
-//                                 -prices[ind] + dp[ind+1][1][cap]);
+//                     profit = max(0+ahead[0][cap], 
+//                                 -prices[ind] + ahead[1][cap]);
 //                  }
     
 //                 if(buy==1){// We can sell the stock
-//                     profit = max(0+dp[ind+1][1][cap],
-//                                 prices[ind] + dp[ind+1][0][cap-1]); 
+//                     profit = max(0+ahead[1][cap],
+//                                 prices[ind] + ahead[0][cap-1]); 
                     
 //                 }
-//                                     dp[ind][buy][cap]=profit;
+//                                     cur[buy][cap]=profit;
 
 //             }
 //         }
-//     }
-        
-        
-        vector<vector<int>> ahead(2,vector<int>(3,0));
-        vector<vector<int>> cur(2,vector<int>(3,0));
-
-          for(int ind = n-1; ind>=0; ind--){
-        for(int buy = 0; buy<2; buy++){
-            for(int cap=1; cap<=2; cap++){
-                
-                int profit=0;
-                if(buy==0){// We can buy the stock
-                    profit = max(0+ahead[0][cap], 
-                                -prices[ind] + ahead[1][cap]);
-                 }
-    
-                if(buy==1){// We can sell the stock
-                    profit = max(0+ahead[1][cap],
-                                prices[ind] + ahead[0][cap-1]); 
-                    
-                }
-                                    cur[buy][cap]=profit;
-
-            }
-        }
           
-          ahead=cur;
-    }
+//           ahead=cur;
+//     }
     
-    return ahead[0][2];
+//     return ahead[0][2];
           
         
     }
