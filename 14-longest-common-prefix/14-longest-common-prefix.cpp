fc05bed623f75c73b95a0ff3d9ea22f2a2e32011
keshav-tyagi28 <89222405+keshav-tyagi28@users.noreply.github.com>
2022-07-16 20:29:41 +0530

    Time: 3 ms (92.12%), Space: 9.2 MB (51.61%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -5,10 +5,11 @@ public:
         string ans="";
         for(int i=0;i<strs[0].size();i++)
         {
+            char ch=strs[0][i];
             bool check=true;
             for(int j=1;j<strs.size();j++)
             {
-                if(strs[0][i]!=strs[j][i])
+                if(strs[j].size()<i || ch!=strs[j][i])
                 {
                     check=false;
                     break;
